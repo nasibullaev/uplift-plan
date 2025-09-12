183501193be2339b8eb51a6d4f736d3e6df92ba7
Fayzullo Nasibullaev <fnasibullayev@gmail.com>
2025-09-12 14:55:17 +0500

    gemini version changed

--- src/ielts/writing-submission/gemini.service.ts
+++ src/ielts/writing-submission/gemini.service.ts
@@ -100,7 +100,7 @@ export class GeminiService {
   }
 
   private async detectEssayStructure(body: string) {
-    const model = this.genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = this.genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
     const structurePrompt = `
 Detect the structure of the following IELTS essay.
